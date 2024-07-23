a7103808009673a339dd5247084d36f434f6e99e
Dasha_Tsumbal <dasha.tsumbal21@gmail.com>
2024-07-23 11:48:23 +0200

    main

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,15 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html' // Перевірте правильність цього імпорту
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
+export default defineConfig({
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        injectData: {
+          // Дані для інжекції
         },
       },
-      outDir: '../dist',
-    },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
-});
+      minify: true,
+    }),
+  ],
+})
